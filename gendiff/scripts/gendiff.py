7ea088d07410a5498bf0b2dcf91e519c0a35d084
AnnaCanada <annazalesova@yandex.ru>
2022-05-22 16:31:27 +0300

    first step done

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,9 +2,12 @@
 import argparse
 
 
-def parse_arguments():
+def parse_cli_arguments():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     args = parser.parse_args()
     return args
+
+if __name__ == '__main__':
+    parse_cli_arguments()
