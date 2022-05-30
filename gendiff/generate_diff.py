1102eb87e370c34e4432b01a79bb15ce9388bede
AnnaCanada <annazalesova@yandex.ru>
2022-05-30 22:38:31 +0300

    3 step

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,32 +2,38 @@
 import json
 
 
-def generate_diff(first_file, second_file):
+def generate_diff(first_file, second_file, format=None):
     file1 = json.load(open(first_file))
-    print(open(first_file))
     file2 = json.load(open(second_file))
-    keys = list(file1.keys() | file2.keys())
-    result = [compare(key, file1, file2) for key in sorted(keys)]
+    keys = sorted(list(file1.keys() | file2.keys()))
+    #result = {key: compare(key, file1, file2) for key in sorted(keys)}
+    result = '{' + '\n'
+    for key in keys:
+        file1_value = file1.get(key)
+        file2_value = file2.get(key)
+        if key in file1 and key in file2 and file1_value == file2_value:
+            result = result + '     ' + str(key) + ':' + str(file1.get(key)) + '\n'
+        if key in file1 and key not in file2:
+            result = result + '    ' + '-' + str(key) + ':' + str(file1.get(key)) + '\n'
+        if key in file2 and key not in file1:
+            result = result + '    ' + '+' + str(key) + ':' + str(file2.get(key)) + '\n'
+        if key in file1 and key in file2 and file1_value != file2_value:
+            result = result + '    ' + '-' + str(key) + ':' + str(file1.get(key)) + '\n'
+            result = result + '    ' + '+' + str(key) + ':' + str(file2.get(key)) + '\n'
+    result = result + '}'
     return result
 
 
-def compare(key, file1, file2):
-    file1_value = file1.get(key)
-    file2_value = file2.get(key)
-    str_key = str(key)
-    str_file2_value = str(file2_value)
-    str_file1_value = str(file1_value)
-    if key in file1 and key in file2 and file1_value == file2_value:
-        string = str_key + ':' + str_file1_value
-        string1 = string + '\n'
-    if key in file1 and key not in file2:
-        string = '+' + str_key + ':' + str_file1_value
-        string1 = string + '\n'
-    if key in file2 and key not in file1:
-        string = '-' + str_key + ':' + str_file2_value
-        string1 = string + '\n'
-    if key in file1 and key in file2 and file1_value != file2_value:
-        string = '+' + str_key + ':' + str_file1_value + '\n' + '-' + str_key + ':' + str_file2_value
-        string1 = string + '\n'
-    return string1
+#def compare(key, file1, file2):
+    #file1_value = file1.get(key)
+    #file2_value = file2.get(key)
+    #if key in file1 and key in file2 and file1_value == file2_value:
+    #    dict = file1.get(key)
+    #if key in file1 and key not in file2:
+    #    dict = file1.get(key)
+    #if key in file2 and key not in file1:
+    #    dict = file2.get(key)
+    #if key in file1 and key in file2 and file1_value != file2_value:
+    #    dict = file2.get(key)
+    #return dict
 
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import argparse
-from gendiff import generate_diff
+
+from gendiff.generate_diff import generate_diff
 
 
 def parse_cli_arguments():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser.add_argument('first_file', type= str)
+    parser.add_argument('second_file', type= str)
     parser.add_argument('-f', '--format', help='set format of output')
 
     args = parser.parse_args()
@@ -14,6 +15,5 @@ def parse_cli_arguments():
 
 
 if __name__ == '__main__':
-    print("hello world")
-    print(generate_diff())
-    parse_cli_arguments()
+    args = parse_cli_arguments()
+    print(generate_diff(args.first_file, args.second_file))
