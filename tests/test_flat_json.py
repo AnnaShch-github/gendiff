74eabe0917f822801e559924d67ea055a3a328a7
AnnaCanada <annazalesova@yandex.ru>
2022-06-06 18:24:32 +0300

    trying workflows

--- tests/test_flat_json.py
+++ tests/test_flat_json.py
@@ -3,6 +3,6 @@ import expected
 
 
 def test_flat_json():
-    verifiable = generate_diff('/home/anna/python-project-lvl2/file1.json',
-                               '/home/anna/python-project-lvl2/file2.json')
+    verifiable = generate_diff('file1.json',
+                               'file2.json')
     assert verifiable == expected.flat_json
